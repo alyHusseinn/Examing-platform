6547751c60dbbfd5595f7c122ec8bd3ef2e2efec
Aly Hussein <111089237+alyHusseinn@users.noreply.github.com>
2025-02-27 14:23:40 +0200

    changing the ChatBot model

--- backend/src/services/gemini.ts
+++ backend/src/services/gemini.ts
@@ -198,7 +198,7 @@ class AIService {
     try {
       const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
       const genAI = new GoogleGenerativeAI(GEMINI_API_KEY!);
-      const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+      const model = genAI.getGenerativeModel({ model: process.env.GEMINI_MODEL! });
 
       const result = await model.generateContent(prompt);
       const response = await result.response;
